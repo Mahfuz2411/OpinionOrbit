774ea64b364245a410234530aaac2951f4c1358e
Mahfuz2411 <mahfuzibnesyful24@gmail.com>
2025-01-08 20:52:55 +0600

    :adhesive_bandage: minor fix

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -1,13 +1,14 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  // Your custom ESLint configuration
   {
     files: ['**/*.{js,mjs,cjs,ts}'],
-    ignores: ['.node_modules/*'],
+    ignores: ['.node_modules/*', '.dist/*'],
     languageOptions: {
       globals: {
         ...globals.browser,
@@ -22,7 +23,11 @@ export default [
       'no-undef': 'error',
     },
   },
+
+  // ESLint recommended configs
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+
+  // Prettier recommended configuration
   eslintPluginPrettierRecommended,
 ]
