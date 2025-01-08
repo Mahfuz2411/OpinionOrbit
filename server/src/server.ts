7bd10e4a0ea2828126d27b1b3baa463c7a8533d7
Mahfuz2411 <mahfuzibnesyful24@gmail.com>
2025-01-08 21:07:57 +0600

    :adhesive_bandage: minor fix

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,17 @@
-import app from "./app";
-import mongoose from 'mongoose';
-import config from "./app/config";
-
+/* eslint-disable no-console */
+import app from './app'
+import config from './app/config'
+import mongoose from 'mongoose'
 
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string);
+    await mongoose.connect(config.database_url as string)
     app.listen(config.port, () => {
-      console.log(`http://localhost:${config.port}`);
-    });
-  } catch(err) {
-    console.log(err);
+      console.log(`http://localhost:${config.port}`)
+    })
+  } catch (err) {
+    console.log(err)
   }
 }
+
+main()
